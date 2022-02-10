a4b3d03f13b3b02df261b5ad0a8c14b560c1e762
Jmelnechuky <melnechuky@hotmail.com>
2022-02-10 20:17:49 +0000

    First Commit with Itellij

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import model.Cat;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        System.out.println(cat);
+
+
+        /*int a = 5;
+        int b = 3;
+
+
+        System.out.println("hi there" + (a+b));
+*/
+    }
+}
