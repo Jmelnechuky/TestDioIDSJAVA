b78d22a6848d09972ad1a12e12e28ce7f59d3dfc
Jmelnechuky <melnechuky@hotmail.com>
2022-02-10 21:32:56 +0000

    First Commit with Itellij

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -9,6 +9,10 @@ public class primeiroPrograma {
         System.out.println(cat);
 
 
+        Livro livro1 = new Livro("The best book",300);
+        System.out.println(livro1);
+
+
         /*int a = 5;
         int b = 3;
 
@@ -17,3 +21,39 @@ public class primeiroPrograma {
 */
     }
 }
+class Livro {
+
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
+
